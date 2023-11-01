bbad693e29818ea28e0405a4d8aea5e444ed848d
Nicholas Rose <47899789+DualOrb@users.noreply.github.com>
2023-11-01 11:25:34 -0400

    Added struct and functions structs for a servo motor

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,48 @@
 
-
-
 fn main() {
 
 }
 
+///Supported Servo Types
+enum ServoType {
+
+}
+
+///Main Servo Struct
+struct Servo {
+    pin: i32,
+
+    min_angle: i32,
+    max_angle: i32,
+
+    servo_type: ServoType,
+
+
+}
+
+impl Servo {
+    ///Initializes all environment variables necessary for the servo
+    pub fn init() {
+
+    }
+
+    ///Sets the angle of the servo to an angle defined in radians
+    pub fn set_angle_deg(angle: &f32) -> Result<(), std::error> {
+        //Checks it is within bounds and passes to generic angle calc
+
+        //Sets the calculated angle of the servo
+    }
+
+    ///Sets the angle of the servo to an angle defined in radians
+    pub fn set_angle_rad(angle: &f32) -> Result<(), std::error> {
+        //Converts to degrees, and passes to the degree angle function
+    }
+
+    ///Calculates the duty cycle and frequency required to achieve the necessary angle
+    fn calculate_angle(angle: &f32) -> (i32, i32) {
+        let duty_cycle = 0;
+        let frequency = 0;
+
+
+    }
+}
