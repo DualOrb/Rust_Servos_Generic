d108baa22282a8cc45afc20de572686985d34c53
Nicholas Rose <47899789+DualOrb@users.noreply.github.com>
2023-11-07 10:46:06 -0500

    Reorganized project

--- src/lib.rs
+++ src/lib.rs
@@ -1,61 +1,55 @@
-use std::f64::consts::PI;
+use crate::servo::Servo;
+mod servo;
 
 fn main() {
 
 }
 
-///Supported Servo Types
-enum ServoType {
-    Miuzuki20kg,
-    Custom
-}
+//Tests
+#[cfg(test)]
+mod tests {
+    use crate::servo::{Servo, ServoType};
 
-///Main Servo Struct
-struct Servo {
-    pin: i32,          //The GPIO pin the servo is connected to
+    #[test]
+    fn test_initialize() {
+        let servo = Servo{
+            pin: 18,
+            min_angle: 0.0,
+            max_angle: 180.0,
+            frequency: 50,
+            min_pulse_width: 2.0,
+            max_pulse_width: 12.0,
 
-    min_angle: f32,    //Minimum and Maximum angle the servo supports (degrees
-    max_angle: f32,
+            servo_type: ServoType::Custom,
+        };
 
-    frequency: i32,    //The frequency in ms that the servo supports (repetition rate)
-
-    min_pulse_width: f32,   //The min and max pulse widths that correspond with the angles inms
-    max_pulse_width: f32,
-
-    servo_type: ServoType,  //A type for a specific servo motor if supported. If not -> type = custom
-}
-
-impl Servo {
-    ///Initializes raspberry pi related services
-    pub fn init() -> Result<(), std::error> {
-
-    }
-
-    ///Sets the angle of the servo to an angle defined in radians
-    pub fn set_angle_deg(&self, angle: &f32) {
-        //Checks it is within bounds and passes to generic angle calc for servo
-        if self.min_angle > *angle && angle > &self.max_angle {
-            panic!("Angle provided is out of bounds");
-        }
-
-        let pulse_width = self.calculate_pulse_widths(angle);
-
-        //Set the GPIO pin with the necessary information
+        assert_eq!(servo.pin, 18);
+        assert_eq!(servo.min_angle, 0.0);
+        assert_eq!(servo.max_angle, 180.0);
+        assert_eq!(servo.frequency, 50);
+        assert_eq!(servo.min_pulse_width, 2.0);
+        assert_eq!(servo.max_pulse_width, 12.0);
+        assert_eq!(servo.servo_type, ServoType::Custom);
 
     }
 
-    ///Sets the angle of the servo to an angle defined in radians
-    pub fn set_angle_rad(&self, angle: &f32) {
-        //Converts to degrees, and passes to the degree angle function
-        let degrees = angle * 180 / PI;
+    #[test]
+    fn test_calc_pulse_width() {
 
-        return self.set_angle_deg(&degrees);
     }
 
-    ///Calculates the pulse widths necessary to achieve the angle
-    fn calculate_pulse_widths(&self, angle: &f32) -> (f32) {
-        return ((self.max_pulse_width - self.min_pulse_width)
-            / self.max_angle * angle)
-            + self.min_pulse_width;
+    #[test]
+    fn test_set_angle_deg() {
+
+    }
+
+    #[test]
+    fn test_set_angle_rad() {
+
     }
 }
+
+
+
+
+
---
+++ src/servo.rs
@@ -0,0 +1,54 @@
+use std::f64::consts::PI;
+
+///Supported Servo Types
+#[derive(Debug, PartialEq, Eq)]
+pub enum ServoType {
+    Miuzuki20kg,
+    Custom
+}
+
+///Main Servo Struct
+pub struct Servo {
+    pub pin: i32,          //The GPIO pin the servo is connected to
+
+    pub min_angle: f32,    //Minimum and Maximum angle the servo supports (degrees
+    pub max_angle: f32,
+
+    pub frequency: i32,    //The frequency in ms that the servo supports (repetition rate)
+
+    pub min_pulse_width: f32,   //The min and max pulse widths that correspond with the angles inms
+    pub max_pulse_width: f32,
+
+    pub servo_type: ServoType,  //A type for a specific servo motor if supported. If not -> type = custom
+}
+
+impl Servo {
+
+    ///Sets the angle of the servo to an angle defined in radians
+    pub fn set_angle_deg(&self, angle: &f32) {
+        //Checks it is within bounds and passes to generic angle calc for servo
+        if self.min_angle > *angle && angle > &self.max_angle {
+            panic!("Angle provided is out of bounds");
+        }
+
+        let pulse_width = self.calculate_pulse_widths(angle);
+
+        //Set the GPIO pin with the necessary information
+
+    }
+
+    ///Sets the angle of the servo to an angle defined in radians
+    pub fn set_angle_rad(&self, angle: &f32) {
+        //Converts to degrees, and passes to the degree angle function
+        let degrees = angle * 180.0 / PI as f32;
+
+        return self.set_angle_deg(&degrees);
+    }
+
+    ///Calculates the pulse widths necessary to achieve the angle
+    fn calculate_pulse_widths(&self, angle: &f32) -> (f32) {
+        return ((self.max_pulse_width - self.min_pulse_width)
+            / self.max_angle * angle)
+            + self.min_pulse_width;
+    }
+}
